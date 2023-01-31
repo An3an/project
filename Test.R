42b98f76e2a59221ea6917e04ca2c32a28aa739b
An3an <andrean337@gmail.com>
2023-01-31 21:10:35 +0000

    Initial commit

---
+++ Test.R
@@ -0,0 +1,14 @@
+
+usethis::use_git_config(user.name = "An3an",
+                        user.email = "andrean337@gmail.com")
+
+# Initiate git:
+usethis::use_git()
+
+
+# Now use:
+gitcreds::gitcreds_set()
+
+
+Print("Hello teamspoRt")
+
