d391f5e8d48aa116ba07477d3ebe219fb42634ad
An3an <andrean337@gmail.com>
2023-02-10 12:58:16 +0000

    ADAE V2

--- programs/adae.R
+++ programs/adae.R
@@ -28,8 +28,8 @@ format_agegr1 <- function(x) {
   case_when(
     x < 65 ~ "<65",
     between(x, 65, 80) ~ "65-80",
-    x > 80 ~ ">80",
-    TRUE ~ "Missing"
+    x > 80 ~ ">80"
+    #TRUE ~ "Missing"
   )
 }
 
@@ -41,10 +41,10 @@ table(adsl$AGE) ##Very useful!
 
 format_agegr1n <- function(x) {
   case_when(
-    x = "<65" ~ 1,
-    x = "65-80" ~ 2,
-    x = ">85" ~ 3,
-    TRUE ~ NA ##Note, missing values
+    x == "<65" ~ 1,
+    x == "65-80" ~ 2,
+    x == ">80" ~ 3
+    #TRUE ~ NA ##Note, missing values
   )
 }
 
@@ -58,10 +58,12 @@ format_racer1n <- function(x) {
     x == "ASIAN" ~ 2,
     x == "BLACK OR AFRICAN AMERICAN" ~ 3,
     x == "NATIVE HAWAIIAN OR OTHER PACIFIC ISLANDER" ~ 5,
-    x == "WHITE" ~ 6,
-    TRUE ~ NA ##Note, missing values
+    x == "WHITE" ~ 6
+    #TRUE ~ NA ##Note, missing values
   )
 }
+
+format_racer1n(adae$RACE)
 
 # Get list of ADSL vars required for ADAE data set
 
@@ -91,7 +93,7 @@ aendt <- derive_vars_dt(
   new_vars_prefix = "AEN",
   AEENDTC,
   highest_imputation = "n",
-  date_imputation = "first",
+  date_imputation = "last", ##Check: use "last" option
   flag_imputation = "auto",
   min_dates = NULL,
   max_dates = NULL,
@@ -113,6 +115,7 @@ adae <- ae %>%
       ASTDY = dy$ASTDY,
       AENDT = aendt$AENDT,
       AENDY =dy$AENDY
+
     )
 
 
@@ -168,6 +171,185 @@ adae$TRTEMFL <- adae$TRTEMFL %>% replace_na('N')
 
 table(adae$TRTEMFL) ##Very useful!
 
+###Deriving 1st Occurrence of Any AE Flag (AOCCFL)
+
+adae <-  restrict_derivation(
+      dataset = adae,
+      derivation = derive_var_extreme_flag,
+      args = params(
+      by_vars = vars(USUBJID, ASTDT,AESEQ),
+      order = vars(ASTDT, AESEQ),
+      new_var = AOCCFL,
+      mode = "first"
+    ),
+    filter = TRTEMFL == "Y"
+  )
+
+##Replacing the NA AOCCFL flag with "N"
+
+adae$AOCCFL <- adae$AOCCFL %>% replace_na('N')
+
+
+###Checking whether all the NA's are replaced with "N"
+
+table(adae$AOCCFL) ##Very useful!
+
+
+###Deriving 1st Occurrence of Any AE Flag (AOCCSFL)
+
+adae <-  restrict_derivation(
+  dataset = adae,
+  derivation = derive_var_extreme_flag,
+  args = params(
+    by_vars = vars(USUBJID, AEBODSYS,ASTDT,AESEQ),
+    order = vars(AEBODSYS,ASTDT, AESEQ),
+    new_var = AOCCSFL,
+    mode = "first"
+  ),
+  filter = TRTEMFL == "Y"
+)
+
+
+##Replacing the NA AOCCSFL flag with "N"
+
+adae$AOCCSFL <- adae$AOCCSFL %>% replace_na('N')
+
+
+###Checking whether all the NA's are replaced with "N"
+
+table(adae$AOCCSFL) ##Very useful!
+
+
+###Deriving 1st Occurrence of Preferred Term Flag (AOCCPFL)
+
+adae <-  restrict_derivation(
+  dataset = adae,
+  derivation = derive_var_extreme_flag,
+  args = params(
+    by_vars = vars(USUBJID, AEBODSYS,AEDECOD,ASTDT,AESEQ),
+    order = vars(AEBODSYS,AEDECOD,ASTDT, AESEQ),
+    new_var = AOCCPFL,
+    mode = "first"
+  ),
+  filter = TRTEMFL == "Y"
+)
+
+
+##Replacing the NA AOCCPFL flag with "N"
+
+adae$AOCCPFL <- adae$AOCCPFL %>% replace_na('N')
+
+table(adae$AOCCPFL) ##Very useful!
+
+
+###Deriving 1st Occurrence of SeriousFlag (AOCC02FL)
+
+adae <-  restrict_derivation(
+  dataset = adae,
+  derivation = derive_var_extreme_flag,
+  args = params(
+    by_vars = vars(USUBJID, ASTDT,AESEQ),
+    order = vars(ASTDT, AESEQ),
+    new_var = AOCC02FL,
+    mode = "first"
+  ),
+  filter = (TRTEMFL == "Y" &
+  AESER == "Y")
+)
+
+
+##Replacing the NA AOCC02FL flag with "N"
+
+adae$AOCC02FL <- adae$AOCC02FL %>% replace_na('N')
+
+table(adae$AOCC02FL) ##Very useful!
+
+
+###Deriving 1st Occurrence of Serious SOC Flag (AOCC03FL)
+
+adae <-  restrict_derivation(
+  dataset = adae,
+  derivation = derive_var_extreme_flag,
+  args = params(
+    by_vars = vars(USUBJID,AEBODSYS, ASTDT,AESEQ),
+    order = vars(AEBODSYS,ASTDT, AESEQ),
+    new_var = AOCC03FL,
+    mode = "first"
+  ),
+  filter = (TRTEMFL == "Y" &
+              AESER == "Y")
+)
+
+
+##Replacing the NA AOCC03FL flag with "N"
+
+adae$AOCC03FL <- adae$AOCC03FL %>% replace_na('N')
+
+table(adae$AOCC03FL) ##Very useful!
+
+
+###Deriving 1st Occurrence of Serious PT Flag (AOCC04FL	)
+
+adae <-  restrict_derivation(
+  dataset = adae,
+  derivation = derive_var_extreme_flag,
+  args = params(
+    by_vars = vars(USUBJID,AEBODSYS,AEDECOD, ASTDT,AESEQ),
+    order = vars(AEBODSYS,AEDECOD,ASTDT, AESEQ),
+    new_var = AOCC04FL	,
+    mode = "first"
+  ),
+  filter = (TRTEMFL == "Y" &
+              AESER == "Y")
+)
+
+
+##Replacing the NA AOCC04FL	 flag with "N"
+
+adae$AOCC04FL	 <- adae$AOCC04FL	 %>% replace_na('N')
+
+table(adae$AOCC04FL	) ##Very useful!
+
+
+##Customized Query 01 Name derivation
+
+`%!in%` <- Negate(`%in%`)
+
+adae$CQ01NAM <-
+  ifelse ((adae$AEDECOD %in% c('APPLICATION', 'DERMATITIS', 'ERYTHEMA', 'BLISTER') |
+             adae$AEBODSYS %in% c('SKIN AND SUBC UTANEOUS TISSUE DISORDERS')) &
+            adae$AEDECOD %!in% c('COLD SWEAT', 'HYPERHIDROSIS', 'ALOPECIA'), 'DERMATOLOGIC EVENTS', NA)
+
+table(adae$CQ01NAM)
+
+#Deriving 1st Occurrence 01 Flag for CQ01 (AOCC01FL)
+
+adae <-  restrict_derivation(
+  dataset = adae,
+  derivation = derive_var_extreme_flag,
+  args = params(
+    by_vars = vars(USUBJID, ASTDT,AESEQ),
+    order = vars(ASTDT, AESEQ),
+    new_var = AOCC01FL	,
+    mode = "first"
+  ),
+  filter = (is.na(CQ01NAM) &
+              TRTEMFL == "Y")
+)
+
+
+##Replacing the NA AOCC01FL	 flag with "N"
+
+table(adae$AOCC01FL	) ##Very useful!
+
+
+adae <- adae %>%
+    mutate(
+    RACEN = format_racer1n(RACE),
+    AGEGR1 = format_agegr1(AGE),
+    AGEGR1N = format_agegr1n(AGEGR1)
+
+)
 
 
 xportr_write(adae, "adam/adae.xpt")
