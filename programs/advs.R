0f68908780a10d807af2d7c9f38c8273b5eb0e45
PavloChaika <chaykivsky@gmail.com>
2023-02-16 12:05:54 +0000

    Final ADVS

--- programs/advs.R
+++ programs/advs.R
@@ -5,7 +5,7 @@
 # Input: adsl, vs
 
 library(admiral)
-library(admiral.test) # Contains example datasets from the CDISC pilot project
+#library(admiral.test) # Contains example datasets from the CDISC pilot project
 library(dplyr)
 library(lubridate)
 library(stringr)
@@ -13,23 +13,62 @@ library(xportr)
 library(haven)
 
 
---test
---test2
 
 vs <- read_xpt("sdtm/vs.xpt")
 adsl <- read_xpt("adam/adsl.xpt")
 
 
 # Read in prepared spec file for ADVS ----
-advs_spec <- readxl::read_xlsx("/cloud/project/metadata/specs.xlsx", sheet = "Variables")  %>%
+advs_spec <- readxl::read_xlsx("./metadata/specs.xlsx", sheet = "Variables")  %>%
   dplyr::rename(type = "Data Type") %>%
   rlang::set_names(tolower) %>%
   mutate(format = str_to_lower(format))  %>%
   filter(dataset == 'ADVS') %>%
-  filter(!variable %in% c('ADVS','AGEGR1', 'AGEGR1N', 'RACEN')) %>%
+  # filter(!variable %in% c('ADVS','AGEGR1', 'AGEGR1N', 'RACEN')) %>%
   mutate(
     type = if_else(type == 'text','character', 'numeric')
   )
+
+
+
+
+format_agegr1 <- function(x) {
+  case_when(
+    x < 65 ~ "<65",
+    between(x, 65, 80) ~ "65-80",
+    x > 80 ~ ">80"
+    #TRUE ~ "Missing"
+  )
+}
+
+table(adsl$AGE) ##Very useful!
+
+
+
+#AGEGR1N
+
+format_agegr1n <- function(x) {
+  case_when(
+    x == "<65" ~ 1,
+    x == "65-80" ~ 2,
+    x == ">80" ~ 3
+    #TRUE ~ NA ##Note, missing values
+  )
+}
+
+#racer1n
+
+format_racer1n <- function(x) {
+  case_when(
+    x == "AMERICAN INDIAN OR ALASKA NATIVE" ~ 6,
+    x == "ASIAN" ~ 5,
+    x == "BLACK OR AFRICAN AMERICAN" ~ 2,
+    x == "NATIVE HAWAIIAN OR OTHER PACIFIC ISLANDER" ~ 4,
+    x == "WHITE" ~ 1
+    #TRUE ~ NA ##Note, missing values
+  )
+}
+
 
 # Handle missing values
 
@@ -67,7 +106,7 @@ derive_vars_dt(
   new_vars_prefix = "A",
   dtc = VSDTC
 ) %>%
-derive_vars_dy(reference_date = TRTSDT, source_vars = vars(ADT))
+  derive_vars_dy(reference_date = TRTSDT, source_vars = vars(ADT))
 
 ## Add PARAMCD and PARAM
 advs_paramcd <- advs_dt_dy %>%
@@ -87,6 +126,7 @@ mutate(
 
 ## Get visit info
 
+
 advs_visit <- advs_paramcd %>%
   # Derive Timing
   mutate(
@@ -94,17 +134,17 @@ advs_visit <- advs_paramcd %>%
     ATPT = VSTPT,
     AVISIT = case_when(
       str_detect(VISIT, "SCREEN|UNSCHED|RETRIEVAL|AMBUL") ~ NA_character_,
+      #str_detect(VISIT, "WEEK 26") ~ "End of Treatment",
       !is.na(VISIT) ~ str_to_title(VISIT),
       TRUE ~ NA_character_
     ),
     AVISITN = as.numeric(case_when(
       VISIT == "BASELINE" ~ "0",
+      #str_detect(VISIT, "WEEK 26") ~ "99",
       str_detect(VISIT, "WEEK") ~ str_trim(str_replace(VISIT, "WEEK", "")),
       TRUE ~ NA_character_
     ))
   )
-
-
 
 
 # Derive baseline flags ----
@@ -112,19 +152,20 @@ advs_btype <- advs_visit %>%
   ## Calculate BASETYPE ----
 derive_var_basetype(
   basetypes = rlang::exprs(
-    "LAST: AFTER LYING DOWN FOR 5 MINUTES" = ATPTN == 815,
-    "LAST: AFTER STANDING FOR 1 MINUTE" = ATPTN == 816,
-    "LAST: AFTER STANDING FOR 3 MINUTES" = ATPTN == 817,
-    "LAST" = is.na(ATPTN)
+    "AFTER LYING DOWN FOR 5 MINUTES" = ATPTN == 815,
+    "AFTER STANDING FOR 1 MINUTE" = ATPTN == 816,
+    "AFTER STANDING FOR 3 MINUTES" = ATPTN == 817,
+    " " = is.na(ATPTN)
   )
 )
 
 
+
 advs_ablfl <- advs_btype %>%
-mutate(
-  ANL01FL = if_else(is.na(AVISITN),NA_character_, 'Y'),
-  ABLFL = VSBLFL
-)
+  mutate(
+    ANL01FL = if_else(is.na(AVISITN),NA_character_, 'Y'),
+    ABLFL = VSBLFL
+  )
 
 ## Derive baseline information ----
 advs_base_chg <- advs_ablfl %>%
@@ -145,9 +186,23 @@ advs_base_chg <- advs_ablfl %>%
   # Calculate PCHG
   derive_var_pchg()
 
+advs_visit_end <- advs_base_chg %>%
+  filter(ANL01FL == "Y") %>%
+  filter(ADT>=TRTSDT, AVISITN > 2) %>%
+  group_by(USUBJID, PARAMCD, BASETYPE) %>%
+  slice_max(ADY) %>%
+  # Derive Timing
+  mutate(
+    ATPTN = VSTPTNUM,
+    ATPT = VSTPT,
+    AVISIT ="End of Treatment",
+    AVISITN = 99
+  ) %>%
+  ungroup()
+
 
 # Add all ADSL variables, apply spec properties, create xpt ----
-advs <- advs_base_chg %>%
+advs <- union(advs_base_chg,advs_visit_end) %>%
   derive_vars_merged(
     dataset_add = select(adsl, !!!negate_vars(adsl_vars)),
     by_vars = vars(STUDYID, USUBJID)) %>%
@@ -155,13 +210,21 @@ advs <- advs_base_chg %>%
     TRTA = TRT01A,
     TRTAN = TRT01AN,
     TRTP = TRT01P,
-    TRTPN = TRT01PN
+    TRTPN = TRT01PN,
+    ADT = date(ADT),
+    RACEN = format_racer1n(RACE),
+    AGEGR1 = format_agegr1(AGE),
+    AGEGR1N = format_agegr1n(AGEGR1)
   ) %>%
   select(advs_spec$variable) %>%
-  xportr_type(advs_spec, "ADVS") %>%
+  #xportr_type(advs_spec, "ADVS") %>%
   xportr_label(advs_spec, "ADVS") %>%
   xportr_format(advs_spec, "ADVS") %>%
   xportr_length(advs_spec, "ADVS") %>%
   xportr_write("adam/advs.xpt", label = "Vital Signs Analysis")
+
+
+
+
 
 
