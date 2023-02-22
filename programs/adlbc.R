43fbd3a7ee224b0ab16db998ca128b72106470cd
PavloChaika <chaykivsky@gmail.com>
2023-02-22 17:41:32 +0000

    ADLBC 2

--- programs/adlbc.R
+++ programs/adlbc.R
@@ -86,12 +86,37 @@ derive_vars_dt(
   derive_vars_dy(reference_date = TRTSDT, source_vars = vars(ADT))
 
 
+# Assign PARAMCD, PARAM, and PARAMN
+
+param_lookup <- tibble::tribble(
+  ~PARAMN, ~PARAM,
+  18,		"Sodium (mmol/L)",
+  19,		"Potassium (mmol/L)",
+  20,		"Chloride (mmol/L)",
+  21,		"Bilirubin (umol/L)",
+  22,		"Alkaline Phosphatase (U/L)",
+  23,		"Gamma Glutamyl Transferase (U/L)",
+  24,		"Alanine Aminotransferase (U/L)",
+  25,		"Aspartate Aminotransferase (U/L)",
+  26,		"Blood Urea Nitrogen (mmol/L)",
+  27,		"Creatinine (umol/L)",
+  28,		"Urate (umol/L)",
+  29,		"Phosphate (mmol/L)",
+  30,		"Calcium (mmol/L)",
+  31,		"Glucose (mmol/L)",
+  32,		"Protein (g/L)",
+  33,		"Albumin (g/L)",
+  34,		"Cholesterol (mmol/L)",
+  35,		"Creatine Kinase (U/L)"
+)
+
+
+
 ## Add PARAMCD and PARAM, Calculate AVAL and AVALC
 adlbc_paramcd <- adlbc_dt_dy %>%
 mutate(
-  PARAM = paste(LBTEST, LBSTRESU, sep = " "),
+  PARAM = paste(LBTEST," (", LBSTRESU,")", sep =''),
   PARAMCD = LBTESTCD,
-  PARAMN = match(PARAM, unique(PARAM)),
   PARCAT1 = "CHEM",
   AVAL = LBSTRESN,
   AVALC = LBSTRESC,
@@ -102,10 +127,26 @@ mutate(
   ANRLO = LBSTNRLO,
   ANRHI = LBSTNRHI,
   ALBTRVAL = pmax(LBSTRESN - (1.5 * A1HI), (0.5 * A1LO) - LBSTRESN),
+) %>%
+derive_vars_merged_lookup(
+  dataset_add = param_lookup,
+  new_vars = vars(PARAMN),
+  by_vars = vars(PARAM),
+  print_not_mapped = TRUE,
+  check_type = "warning"
 )
 
 adlbc_anrind <- adlbc_paramcd %>%
-  derive_var_anrind()
+  derive_var_anrind() %>%
+  mutate(
+    ANRIND = case_when(
+      ANRIND == "NORMAL" ~ "N",
+      ANRIND == "LOW" ~ "L",
+      ANRIND == "HIGH" ~ "H",
+      TRUE ~ NA_character_
+    )
+  )
+
 
 
 ## Get visit info
@@ -113,12 +154,13 @@ adlbc_anrind <- adlbc_paramcd %>%
 adlbc_visit <- adlbc_anrind %>%
   mutate(
     AVISIT = case_when(
+      LBBLFL == "Y" ~ "Baseline",
       str_detect(VISIT, "SCREEN|UNSCHED|RETRIEVAL|AMBUL") ~ NA_character_,
       !is.na(VISIT) ~ str_to_title(VISIT),
       TRUE ~ NA_character_
     ),
     AVISITN = as.numeric(case_when(
-      VISIT == "BASELINE" ~ "0",
+      LBBLFL == "Y" ~ "0",
       str_detect(VISIT, "WEEK") ~ str_trim(str_replace(VISIT, "WEEK", "")),
       TRUE ~ NA_character_
     ))
@@ -128,7 +170,19 @@ adlbc_visit <- adlbc_anrind %>%
 adlbc_ablfl <- adlbc_visit %>%
   mutate(
     ANL01FL = if_else(is.na(AVISITN),NA_character_, 'Y'),
-    ABLFL = LBBLFL
+    ABLFL = LBBLFL,
+
+  ) %>%
+  # Last value in treatment visit
+  restrict_derivation(
+    derivation = derive_var_extreme_flag,
+    args = params(
+      by_vars = vars(STUDYID, USUBJID, PARAMCD),
+      order = vars(VISITNUM, ADT),
+      new_var = AENTMTFL,
+      mode = "last"
+    ),
+    filter = (AVISITN>0 & AVISITN<=24)
   )
 
 ## Derive baseline information ----
@@ -146,9 +200,12 @@ adlbc_base_chg <- adlbc_ablfl %>%
     new_var = BASEC
   ) %>%
   # Calculate CHG
-  derive_var_chg() %>%
-  # Calculate PCHG
-  derive_var_pchg() %>%
+  mutate(
+    CHG = case_when(
+      ABLFL=="Y" ~ NA,
+      TRUE ~ AVAL - BASE
+    )
+    )%>%
   # Calculate BNRIND
   derive_var_base(
     by_vars = vars(STUDYID, USUBJID, PARAMCD),
@@ -168,9 +225,20 @@ adlbc_base_chg <- adlbc_ablfl %>%
     new_var = BR2A1HI
   )
 
+adlbc_visit_end <- adlbc_base_chg %>%
+  filter(ANL01FL == "Y") %>%
+  filter(ADT>=TRTSDT,AVISITN < 26, AVISITN >= 2) %>%
+  group_by(USUBJID, PARAMCD) %>%
+  slice_max(ADY) %>%
+  # Derive Timing
+  mutate(
+    AVISIT ="End of Treatment",
+    AVISITN = 99
+  ) %>%
+  ungroup()
 
 # Add all ADSL variables, apply spec properties, create xpt ----
-adlbc <- adlbc_base_chg %>%
+adlbc <- union(adlbc_base_chg,adlbc_visit_end) %>%
   derive_vars_merged(
     dataset_add = select(adsl, !!!negate_vars(adsl_vars)),
     by_vars = vars(STUDYID, USUBJID)) %>%
@@ -184,11 +252,15 @@ adlbc <- adlbc_base_chg %>%
     AGEGR1N = format_agegr1n(AGEGR1)
   ) %>%
   select(adlbc_spec$variable) %>%
-  #xportr_type(adlbc_spec, "ADLB") %>%
-  xportr_label(adlbc_spec, "ADLB") %>%
-  xportr_format(adlbc_spec, "ADLB") %>%
-  xportr_length(adlbc_spec, "ADLB") %>%
+  #xportr_type(adlbc_spec, "ADLBC") %>%
+  xportr_label(adlbc_spec, "ADLBC") %>%
+  xportr_format(adlbc_spec, "ADLBC") %>%
+  xportr_length(adlbc_spec, "ADLBC") %>%
   xportr_write("adam/adlbc.xpt", label = "Analysis Dataset Lab Blood Chemistry")
+
+
+
+check <- adlbc %>% distinct(PARAM, PARAMN)
 
 
 
