cfea70bc50a0706ebb5bea5d6d08ef084ba30050
rhiannonl4wson <lawsonrhiannon@gmail.com>
2023-02-27 16:03:30 +0000

    ADTTE 7/10

--- programs/adtte.R
+++ programs/adtte.R
@@ -23,12 +23,47 @@ library(xportr)
 adsl <- read_xpt("adam/adsl.xpt")
 adae <- read_xpt("adam/adae.xpt")
 
-#Creating shell of program before ADAE is complete
-adsl_vars<-c("AGE","RACE","SAFFL","SEX","SITEID","RFSTDTC","STUDYID","TRT01A",
-             "TRT01AN","TRTEDT","TRT01P","TRTSDT","USUBJID","RFENDT")
+#Creating variables
+#AGEGR1
 
-#"AGEGR1" ,"AGEGR1N","RACEN","TRTDUR"add later
-adae_vars<-c("ASTDT","USUBJID","TRTEMFL","STUDYID","USUBJID","SITEID","AEDECOD","AESEQ","CQ01NAM","AGEGR1" ,"AGEGR1N","RACEN","TRTDUR")
+format_agegr1 <- function(x) {
+  case_when(
+    x < 65 ~ "<65",
+    between(x, 65, 80) ~ "65-80",
+    x > 80 ~ ">80"
+    #TRUE ~ "Missing"
+  )
+}
+
+
+#AGEGR1N
+
+format_agegr1n <- function(x) {
+  case_when(
+    x == "<65" ~ 1,
+    x == "65-80" ~ 2,
+    x == ">80" ~ 3
+    #TRUE ~ NA ##Note, missing values
+  )
+}
+
+#racer1n
+
+format_racer1n <- function(x) {
+  case_when(
+    x == "AMERICAN INDIAN OR ALASKA NATIVE" ~ 6,
+    x == "ASIAN" ~ 3,
+    x == "BLACK OR AFRICAN AMERICAN" ~ 2,
+    x == "NATIVE HAWAIIAN OR OTHER PACIFIC ISLANDER" ~ 5,
+    x == "WHITE" ~ 1
+    #TRUE ~ NA ##Note, missing values
+  )
+}
+
+#put TRTP/TRTA here for now (FIX if have time)
+adsl<-adsl%>%mutate(RACEN = format_racer1n(RACE),
+             AGEGR1 = format_agegr1(AGE),
+             AGEGR1N = format_agegr1n(AGEGR1),  RFENDT = as.Date(RFENDT, origin = "1970-01-01"))
 
 #Functions from admiral (just for reference):
 #censor_source,convert_blanks_to_na,derive_param_tte,list_tte_source_objects,params,tte_source
@@ -54,18 +89,17 @@ param_lookup <- tibble::tribble(
 
 
 # Get list of ADSL vars required for derivations
-adsl_vars1<-vars(RFSTDTC,STUDYID,USUBJID,RFENDT,SITEID)
+adsl_vars1<-vars(RFSTDTC,STUDYID,USUBJID,RFENDT,SITEID,TRTSDT,TRTEDT)
 
 # adae_vars1<-vars(ASTDT,USUBJID,TRTEMFL,STUDYID,USUBJID,SITEID,AEDECOD,"AESEQ","CQ01NAM")
 
 
-#Merge together adsl and adae
-work_adtte <- adae %>%
+#Merge together adsl and adae to be used in param derivation
+work_adtte <- adae%>%filter(TRTEMFL=="Y") %>%
   derive_vars_merged(
     dataset_add = adsl,
     new_vars = adsl_vars1,
-    by_vars = vars(STUDYID,USUBJID,SITEID),
-    #filter_add = CQ01NAM == "DERMATOLOGIC EVENTS",
+    by_vars = vars(STUDYID,USUBJID,SITEID,TRTSDT,TRTEDT),
   )
 
 
@@ -101,7 +135,7 @@ eos <- censor_source(
 param_tte<-derive_param_tte(
   dataset_adsl = adsl,
   source_datasets = list(adsl = adsl, adae = adae),
-  start_date = TRTSDT,
+  # start_date = TRTSDT,
   event_conditions = list(ttae),
   censor_conditions = list(eos),
   subject_keys = vars(USUBJID,STUDYID,SITEID),
@@ -117,25 +151,39 @@ param_tte1<-param_tte %>% mutate(
 
 #merge our parameters back onto original data to get all the variables we need
 
-adtte_vars=vars(EVNTDESC,SRCDOM,SRCSEQ,CNSR,ADT,STARTDT,PARAMCD,PARAM,AVAL)
 
-adtte_<-left_join(param_tte1,work_adtte,by=c("USUBJID","STUDYID","SITEID"),multiple="all")
+adtte_vars=vars(EVNTDESC,SRCDOM,SRCSEQ,CNSR,ADT,STARTDT,PARAMCD,PARAM,AVAL,SRCVAR)
 
-#taking first obs (check spec)
-adtte_<-adtte_%>%arrange(USUBJID,ASTDT)%>%group_by(USUBJID)%>%slice(1)
 
-#using the spec to include only the variables we need
 
-#update to all_of when we have all the variables needed in ADSL/ADAE
+adtte_<-adsl%>%derive_vars_merged(
+  dataset_add = param_tte1,
+  new_vars = adtte_vars,
+  by_vars = vars(STUDYID,USUBJID,SITEID),
+)
 
 variables<-adtte_spec$variable
 adtte_<-convert_blanks_to_na(adtte_)
 
-adtte<-adtte_%>%select(any_of(variables))%>%
+adtte<-adtte_%>%
   xportr_type(adtte_spec, "ADTTE") %>%
   xportr_label(adtte_spec, "ADTTE") %>%
-  xportr_format(adtte_spec, "ADTTE") %>%
-  xportr_length(adtte_spec, "ADTTE") %>%
+  xportr_format(adtte_spec, "ADTTE")%>%
+  xportr_length(adtte_spec, "ADTTE")%>%
+  mutate(
+    TRTSDT = as.Date(TRTSDT, origin = "1970-01-01"),
+    TRTEDT = as.Date(TRTEDT, origin = "1970-01-01"),
+    ADT = as.Date(ADT, origin = "1970-01-01"),
+    STARTDT = as.Date(STARTDT, origin = "1970-01-01"),
+    TRTP = TRT01P,
+    TRTPN = TRT01PN,
+    TRTDUR=TRTDURD,
+    TRTA = TRT01A,
+    TRTAN = TRT01AN)%>% select(all_of(variables))
+
+
+adtte<-adtte%>%
   xportr_write("adam/adtte.xpt", label = "AE Time To 1st Derm. Event Analysis")
 
+final_adtte<-read_xpt("adam/adtte.xpt")
 
